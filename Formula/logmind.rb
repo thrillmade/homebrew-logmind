f31f980dc3b620e6f0132c62e202a649a7f34759
thrillmot <thrillmot@users.noreply.github.com>
2026-05-26 23:54:49 -0400

    logmind 0.3.1

--- Formula/logmind.rb
+++ Formula/logmind.rb
@@ -3,8 +3,8 @@ class Logmind < Formula
 
   desc "Branch-aware AI decision logging for development projects"
   homepage "https://logmind.dev"
-  url "https://files.pythonhosted.org/packages/da/75/6ba83a4d475fc78f957dc0495bc0feca83856ceeed82bc299ec9e63ebccb/logmind-0.3.0.tar.gz"
-  sha256 "4d70f9956987571a812f495d6e9ae9e3e70552ba3b1a047fc0f28e2ad5091172"
+  url "https://files.pythonhosted.org/packages/85/24/6a5af6b4c225243e3c30afcf6dff52cabcb69286a01beb1c0ed6f35b9fdc/logmind-0.3.1.tar.gz"
+  sha256 "565ce28e3d0a52b2e222e590d4f36c45117e6099f5efebce25e2e89787f17e24"
   license "MIT"
   head "https://github.com/thrillmade/logmind.git", branch: "main"
 
@@ -12,12 +12,12 @@ class Logmind < Formula
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/bb/63/f9e1ea081ce35720d8b92acde70daaedace594dc93b693c869e0d5910718/click-8.3.3.tar.gz"
-    sha256 "398329ad4837b2ff7cbe1dd166a4c0f8900c3ca3a218de04466f38f6497f18a2"
+    sha256 "565ce28e3d0a52b2e222e590d4f36c45117e6099f5efebce25e2e89787f17e24"
   end
 
   resource "pyyaml" do
     url "https://files.pythonhosted.org/packages/05/8e/961c0007c59b8dd7729d542c61a4d537767a59645b82a0b521206e1e25c2/pyyaml-6.0.3.tar.gz"
-    sha256 "d76623373421df22fb4cf8817020cbb7ef15c725b9d5e45f17e189bfc384190f"
+    sha256 "565ce28e3d0a52b2e222e590d4f36c45117e6099f5efebce25e2e89787f17e24"
   end
 
   def install
